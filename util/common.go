dd969b9796f2bb458281f5d892c4b153c784bf2a
zhoufuture <zhoufuture@126.com>
2016-03-03 15:02:35 +0800

    add CheckError in util package

--- util/common.go
+++ util/common.go
@@ -3,10 +3,20 @@ package util
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"os"
+
+	"github.com/zhoufuture/golite/logger"
 )
 
 func GetMd5String(s string) string {
 	h := md5.New()
 	h.Write([]byte(s))
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+func CheckError(err error) {
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 }
